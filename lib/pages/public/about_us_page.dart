b0446b7933191d8ed9c9f2310e7b1da445937676
Pedro Procter <oprocter89@gmail.com>
2022-12-13 14:26:44 -0400

    feat: Change DeveloperProfileSection 2 Photo Profile

--- lib/pages/public/about_us_page.dart
+++ lib/pages/public/about_us_page.dart
@@ -64,7 +64,7 @@ class AboutUsPage extends StatelessWidget {
                 phoneNumber: '+18299386415',
                 telegramUrl: 'https://telegram.me/pedroprocter'),
             DeveloperProfileSection(
-                photoPath: 'assets/images/photo_profile_d1.jpg',
+                photoPath: 'assets/images/photo_profile_d2.jpeg',
                 name: 'Miguel Higuera',
                 description:
                     """Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""",
