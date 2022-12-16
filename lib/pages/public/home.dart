f3bb19de3fde48a92724ccdb93465bc835ce6808
Miguel Higuera <migueladolfohiguera@hotmail.com>
2022-12-15 21:02:31 -0400

    Login

--- lib/pages/public/home.dart
+++ lib/pages/public/home.dart
@@ -1,4 +1,5 @@
 import 'package:civil_defense_app/pages/public/about_us_page.dart';
+import 'package:civil_defense_app/pages/public/history_page.dart';
 import 'package:civil_defense_app/pages/public/login.dart';
 import 'package:civil_defense_app/pages/public/members_page.dart';
 import 'package:civil_defense_app/pages/public/news_page.dart';
@@ -8,7 +9,6 @@ import 'package:civil_defense_app/pages/public/videos_page.dart';
 import 'package:civil_defense_app/pages/public/become_volunteer.dart';
 import 'package:flutter/material.dart';
 
-import 'history.dart';
 import 'shelters_map_page.dart';
 
 class Home extends StatelessWidget {
@@ -21,7 +21,7 @@ class Home extends StatelessWidget {
           title: const Text(''),
           backgroundColor: const Color(0xfffd6c00),
         ),
-        drawer: const appDrawer(),
+        drawer: const DrawerWithoutLogin(),
         body: Center(
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -67,7 +67,8 @@ class appDrawer extends StatelessWidget {
                   margin: const EdgeInsets.only(top: 0),
                   padding: const EdgeInsets.only(left: 15),
                   width: double.infinity,
-                  child: const Text('Noticias'),
+                  child: const Text('Noticias',
+                      style: TextStyle(color: Colors.white)),
                 ),
                 onTap: () {
                   Navigator.push(
@@ -83,7 +84,8 @@ class appDrawer extends StatelessWidget {
                   margin: const EdgeInsets.only(top: 15),
                   padding: const EdgeInsets.only(left: 15),
                   width: double.infinity,
-                  child: const Text('Historia'),
+                  child: const Text('Historia',
+                      style: TextStyle(color: Colors.white)),
                 ),
                 onTap: () {
                   Navigator.push(
@@ -99,7 +101,8 @@ class appDrawer extends StatelessWidget {
                   margin: const EdgeInsets.only(top: 15),
                   padding: const EdgeInsets.only(left: 15),
                   width: double.infinity,
-                  child: const Text('Medidas Preventivas'),
+                  child: const Text('Medidas Preventivas',
+                      style: TextStyle(color: Colors.white)),
                 ),
                 onTap: () {
                   Navigator.push(
@@ -116,7 +119,8 @@ class appDrawer extends StatelessWidget {
                   margin: const EdgeInsets.only(top: 15),
                   padding: const EdgeInsets.only(left: 15),
                   width: double.infinity,
-                  child: const Text('Miembros'),
+                  child: const Text('Miembros',
+                      style: TextStyle(color: Colors.white)),
                 ),
                 onTap: () {
                   Navigator.push(
@@ -132,7 +136,8 @@ class appDrawer extends StatelessWidget {
                   margin: const EdgeInsets.only(top: 15),
                   padding: const EdgeInsets.only(left: 15),
                   width: double.infinity,
-                  child: const Text('Albergues'),
+                  child: const Text('Albergues',
+                      style: TextStyle(color: Colors.white)),
                 ),
                 onTap: () {
                   Navigator.push(
@@ -148,7 +153,8 @@ class appDrawer extends StatelessWidget {
                   margin: const EdgeInsets.only(top: 15),
                   padding: const EdgeInsets.only(left: 15),
                   width: double.infinity,
-                  child: const Text('Mapas'),
+                  child: const Text('Mapas',
+                      style: TextStyle(color: Colors.white)),
                 ),
                 onTap: () {
                   Navigator.push(
@@ -164,7 +170,8 @@ class appDrawer extends StatelessWidget {
                   margin: const EdgeInsets.only(top: 15),
                   padding: const EdgeInsets.only(left: 15),
                   width: double.infinity,
-                  child: const Text('Videos'),
+                  child: const Text('Videos',
+                      style: TextStyle(color: Colors.white)),
                 ),
                 onTap: () {
                   Navigator.push(
@@ -180,7 +187,8 @@ class appDrawer extends StatelessWidget {
                   margin: const EdgeInsets.only(top: 15),
                   padding: const EdgeInsets.only(left: 15),
                   width: double.infinity,
-                  child: const Text('Quiero ser voluntario'),
+                  child: const Text('Quiero ser voluntario',
+                      style: TextStyle(color: Colors.white)),
                 ),
                 onTap: () {
                   Navigator.push(context,
@@ -194,7 +202,8 @@ class appDrawer extends StatelessWidget {
                   margin: const EdgeInsets.only(top: 15),
                   padding: const EdgeInsets.only(left: 15),
                   width: double.infinity,
-                  child: const Text('Sobre nosotros'),
+                  child: const Text('Sobre nosotros',
+                      style: TextStyle(color: Colors.white)),
                 ),
                 onTap: () {
                   Navigator.push(
@@ -204,13 +213,148 @@ class appDrawer extends StatelessWidget {
                 },
               ),
             ),
+            // SizedBox(
+            //   child: InkWell(
+            //     child: Container(
+            //       margin: const EdgeInsets.only(top: 15),
+            //       padding: const EdgeInsets.only(left: 15),
+            //       width: double.infinity,
+            //       child: const Text('Login'),
+            //     ),
+            //     onTap: () {
+            //       Navigator.push(context,
+            //           MaterialPageRoute(builder: (context) => LoginPage()));
+            //     },
+            //   ),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//Drawer if the user isn't login
+class DrawerWithoutLogin extends StatelessWidget {
+  const DrawerWithoutLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Color(0xffD06224),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                margin: const EdgeInsets.only(top: 30),
+                child: Image.asset(
+                  'assets/images/defensa_civil_logo.png',
+                  width: 200,
+                ),
+              ),
+            ),
+            SizedBox(
+              child: InkWell(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 0),
+                  padding: const EdgeInsets.only(left: 15),
+                  width: double.infinity,
+                  child: const Text('Noticias',
+                      style: TextStyle(color: Colors.white)),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => NewsPage()));
+                },
+              ),
+            ),
             SizedBox(
               child: InkWell(
                 child: Container(
                   margin: const EdgeInsets.only(top: 15),
                   padding: const EdgeInsets.only(left: 15),
                   width: double.infinity,
-                  child: const Text('Login'),
+                  child: const Text('Historia',
+                      style: TextStyle(color: Colors.white)),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HistoryPage()));
+                },
+              ),
+            ),
+            SizedBox(
+              child: InkWell(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 15),
+                  padding: const EdgeInsets.only(left: 15),
+                  width: double.infinity,
+                  child: const Text('Medidas Preventivas',
+                      style: TextStyle(color: Colors.white)),
+                ),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => PreventiveMeasuresPage()));
+                },
+              ),
+            ),
+            SizedBox(
+              child: InkWell(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 15),
+                  padding: const EdgeInsets.only(left: 15),
+                  width: double.infinity,
+                  child: const Text('Miembros',
+                      style: TextStyle(color: Colors.white)),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MembersPage()));
+                },
+              ),
+            ),
+            SizedBox(
+              child: InkWell(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 15),
+                  padding: const EdgeInsets.only(left: 15),
+                  width: double.infinity,
+                  child: const Text('Videos',
+                      style: TextStyle(color: Colors.white)),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => VideosPage()));
+                },
+              ),
+            ),
+            SizedBox(
+              child: InkWell(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 15),
+                  padding: const EdgeInsets.only(left: 15),
+                  width: double.infinity,
+                  child: const Text('Sobre nosotros',
+                      style: TextStyle(color: Colors.white)),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => AboutUsPage()));
+                },
+              ),
+            ),
+            SizedBox(
+              child: InkWell(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 15),
+                  padding: const EdgeInsets.only(left: 15),
+                  width: double.infinity,
+                  child: const Text('Iniciar sesión',
+                      style: TextStyle(color: Colors.white)),
                 ),
                 onTap: () {
                   Navigator.push(context,
---
+++ lib/pages/public/report_situation.dart
@@ -0,0 +1,280 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'home.dart';
+
+class ReportSituationPage extends StatefulWidget {
+  static String id = 'login_page';
+
+  @override
+  _ReportSituationPageState createState() => _ReportSituationPageState();
+}
+
+class _ReportSituationPageState extends State<ReportSituationPage> {
+  String selectedImagePath = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+            title: Text('Reportar situación'),
+            backgroundColor: Color(0xfffd6c00)),
+        body: Center(
+          child: Column(
+            children: [
+              _titleTextField(),
+              SizedBox(
+                height: 15,
+              ),
+              _descriptionTextField(),
+              _latitudeTextField(),
+              SizedBox(
+                height: 20,
+              ),
+              _lengthTextField(),
+              SizedBox(
+                height: 20,
+              ),
+              //Declare photo selector
+              SizedBox(
+                height: 20,
+              ),
+              selectedImagePath == ''
+                  ? Image.asset(
+                      'assets/images/defensa_civil_logo.png',
+                      height: 200,
+                      width: 200,
+                      fit: BoxFit.fill,
+                    )
+                  : Image.file(
+                      File(selectedImagePath),
+                      height: 200,
+                      width: 200,
+                      fit: BoxFit.fill,
+                    ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Selecciona una imagen',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xfffd6c00))),
+                child: Text('Seleccionar'),
+                onPressed: () {
+                  selectImage();
+                  setState(() {});
+                },
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              _setData(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+// Photo selector
+  Future selectImage() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            child: Container(
+              height: 200,
+              child: Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Column(
+                  children: [
+                    Text('Selecciona una imagen desde',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        GestureDetector(
+                          onTap: () async {
+                            selectedImagePath = await selectImageFromGallery();
+                            print('Image_Path:-');
+                            print(selectedImagePath);
+                            if (selectedImagePath != '') {
+                              Navigator.pop(context);
+                              setState(() {});
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      content: Text(
+                                          'No se ha seleccionado una imagen')));
+                            }
+                          },
+                          child: Card(
+                            elevation: 5,
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Image.asset('assets/images/gallery_icon.png',
+                                      height: 60, width: 60),
+                                  Text('Galería',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold))
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () async {
+                            selectedImagePath = await selectImageFromCamera();
+                            print('Image_Path:-');
+                            print(selectedImagePath);
+                            if (selectedImagePath != '') {
+                              Navigator.pop(context);
+                              setState(() {});
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      content: Text(
+                                          'No se ha seleccionado una imagen')));
+                            }
+                          },
+                          child: Card(
+                            elevation: 5,
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Image.asset('assets/images/camara_icon.jpg',
+                                      height: 60, width: 60),
+                                  Text('Cámara',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold))
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+
+//Photo field functions
+  selectImageFromGallery() async {
+    XFile? file = await ImagePicker()
+        .pickImage(source: ImageSource.gallery, imageQuality: 10);
+    if (file != null) {
+      return file.path;
+    } else {
+      return '';
+    }
+  }
+
+  selectImageFromCamera() async {
+    XFile? file = await ImagePicker()
+        .pickImage(source: ImageSource.camera, imageQuality: 10);
+    if (file != null) {
+      return file.path;
+    } else {
+      return '';
+    }
+  }
+
+  Widget _titleTextField() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: TextField(
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+              icon: Icon(Icons.title), hintText: 'Título', labelText: 'Título'),
+          onChanged: ((value) {}),
+        ),
+      );
+    });
+  }
+
+  Widget _descriptionTextField() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: TextField(
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+              icon: Icon(Icons.description),
+              hintText: 'Descripción',
+              labelText: 'Descripción'),
+          onChanged: ((value) {}),
+        ),
+      );
+    });
+  }
+
+  Widget _latitudeTextField() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+              icon: Icon(Icons.location_city),
+              hintText: '00.0000000',
+              labelText: 'Latitud'),
+          onChanged: ((value) {}),
+        ),
+      );
+    });
+  }
+
+  Widget _lengthTextField() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+              icon: Icon(Icons.location_city),
+              hintText: '00.0000000',
+              labelText: 'Longitud'),
+          onChanged: ((value) {}),
+        ),
+      );
+    });
+  }
+
+  Widget _setData() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return TextButton(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+          child: Text('Enviar', style: TextStyle(color: Color(0xFFFEFEFF))),
+          color: Color(0xfffd6c00),
+        ),
+        onPressed: () {},
+      );
+    });
+  }
+}
